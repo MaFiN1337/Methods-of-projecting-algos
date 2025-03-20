f752a1f3a8a477489bd9086f05fed5373a550af5
Anton <146825794+MaFiN1337@users.noreply.github.com>
2025-03-20 14:07:57 +0200

    Update Find-sums.py
    Delete result argument in function and just return the result of two arrays merged.

--- Find-sums.py
+++ Find-sums.py
@@ -1,17 +1,10 @@
-def find_needed_sum(arr, S, current_sum = 0, subset=None, result=None):
-    if result is None:
-        result = []
+def find_needed_sum(arr, S, current_sum = 0, subset=None):
     if subset is None:
         subset = []
     if current_sum == S:
-        return subset
+        return [subset]
     if current_sum > S or not arr:
-        return None
-    with_elem = find_needed_sum(arr[1:], S, arr[0] + current_sum, subset + [arr[0]], result)
-    if with_elem and (with_elem[0] not in result):
-        result.append(with_elem)
-    without_elem = find_needed_sum(arr[1:], S, current_sum, subset, result)
-    if without_elem and (without_elem[0] not in result):
-        result.append(without_elem)
-    return result
-# How to modify it without using result?
+        return []
+    with_elem = find_needed_sum(arr[1:], S, arr[0] + current_sum, subset + [arr[0]])
+    without_elem = find_needed_sum(arr[1:], S, current_sum, subset)
+    return with_elem + without_elem
