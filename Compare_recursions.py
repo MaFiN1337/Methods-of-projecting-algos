dcd21a34e6624358e9f897fb57f819b03e426232
Anton <146825794+MaFiN1337@users.noreply.github.com>
2025-03-30 11:04:02 +0300

    Add files via upload
    Compare_recursions.py is a file that compares recursive+insert merge sort and recursive merge sort over time, using time.perf_counter() from time module.

---
+++ Compare_recursions.py
@@ -0,0 +1,14 @@
+import time
+
+def main():
+    reversed_small_arr = []
+    reversed_large_arr = []
+    sorted_small_arr = []
+    sorted_large_arr = []
+    with_doubles_small_arr = []
+    with_doubles_large_arr = []
+    random_small_arr = []
+    random_large_arr = []
+
+if __name__ == '__main__':
+    main()
---
+++ Recursive+insert_merge_sort.py
@@ -0,0 +1,37 @@
+def recurse_merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    if len(arr) < 7:
+        return insert_sort(arr)
+    left_side = arr[:len(arr)//2]
+    right_side = arr[len(arr)//2:]
+    left_arr = recurse_merge_sort(left_side)
+    right_arr = recurse_merge_sort(right_side)
+    return merge(left_arr, right_arr)
+
+def merge(left_arr, right_arr):
+    result = []
+    while left_arr and right_arr:
+        if left_arr[0] < right_arr[0]:
+            result.append(left_arr[0])
+            left_arr = left_arr[1:]
+        else:
+            result.append(right_arr[0])
+            right_arr = right_arr[1:]
+    result.extend(left_arr)
+    result.extend(right_arr)
+    return result
+
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+print(recurse_merge_sort([1,2,3,4,5,6,7,8,9,10]))
+print(recurse_merge_sort([5, 3, 7, 1, 8]))
+print(recurse_merge_sort([9, 3, 1, 18, 50]))
